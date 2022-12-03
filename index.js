537ad7cc906ee94328826b50bdba5033ef3a1c10
San Do <san.do@sapotacorp.vn>
2022-12-03 13:02:19 +0700

    morgan

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'));
 app.get('/123', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c =  a + b;
   res.send('Hello World!')
 })
 
